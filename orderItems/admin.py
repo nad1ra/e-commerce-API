c3abe2ce49ff44c0f718e01cf89564eb7c7abdaa
nad1ra <nodirakamaliddinovna@gamil.com>
2025-04-15 16:01:16 +0500

    done

--- orderItems/admin.py
+++ orderItems/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import OrderItem
 
-# Register your models here.
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'price')
+    search_fields = ('product', 'price')
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'customer_email', 'customer_phone')
+    search_fields = ('customer_name', 'customer_email', 'customer_phone')
+
+
